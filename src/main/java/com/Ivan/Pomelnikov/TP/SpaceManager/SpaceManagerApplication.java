52459f82f58db8de88f956d5506ca37f63c3ea82
Ivan Pomelnikov <vanidzepomelnikov@gmail.com>
2021-12-26 13:01:16 +0400

    Initial commit

---
+++ src/main/java/com/Ivan/Pomelnikov/TP/SpaceManager/SpaceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.Ivan.Pomelnikov.TP.SpaceManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ivan/Pomelnikov/TP/SpaceManager/SpaceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ivan.Pomelnikov.TP.SpaceManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
